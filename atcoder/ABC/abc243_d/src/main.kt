e8eadedcc5769d00cf96b42c9451ce2b7f336082
Trabarc <trabarc@hotmail.com>
2022-03-12 22:58:27 +0900

    20分オーバーでD問題までクリア

--- atcoder/ABC/abc243_d/src/main.kt
+++ atcoder/ABC/abc243_d/src/main.kt
@@ -1,15 +1,49 @@
 import java.io.PrintWriter
+
 fun next() = readLine()!!
-fun nextInt() = next().toInt()
-fun nextLong() = next().toLong()
-fun nextDouble() = next().toDouble()
-fun nextList() = next().split(" ")
-fun nextIntList() = next().split(" ").map{ it.toInt() }
 fun nextLongList() = next().split(" ").map{ it.toLong() }
-fun nextDoubleList() = next().split(" ").map{ it.toDouble() }
 
 @JvmField val _writer = PrintWriter(System.out, false)
+@kotlin.ExperimentalStdlibApi
 fun main() { _writer.solve(); _writer.flush() }
+@kotlin.ExperimentalStdlibApi
 fun PrintWriter.solve() {
+    val (n, x) = nextLongList()
 
+    val s = next()
+
+    val que = ArrayDeque<Char>()
+
+    for (i in s) {
+        when (i) {
+            'U' -> {
+                if (que.isNotEmpty() && (que.last() == 'L' || que.last() == 'R')) {
+                    que.removeLast()
+                }
+                else {
+                    que.add('U')
+                }
+
+            }
+
+            'L' -> {
+                que.add('L')
+            }
+
+            'R' -> {
+                que.add('R')
+            }
+        }
+    }
+
+    var ans = x
+    for (i in que) {
+        when (i) {
+            'L' -> ans *= 2
+            'R' -> ans = ans * 2 + 1
+            'U' -> ans /= 2
+        }
+    }
+
+    println(ans)
 }
